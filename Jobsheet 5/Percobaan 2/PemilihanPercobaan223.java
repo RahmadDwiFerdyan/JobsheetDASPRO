aa1986c664e1fc6d69049760179b368c047c0a11
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-09-27 08:20:45 +0700

    kode setelah modifikasi

--- Jobsheet 5/Percobaan 2/PemilihanPercobaan223.java
+++ Jobsheet 5/Percobaan 2/PemilihanPercobaan223.java
@@ -15,9 +15,28 @@ public class PemilihanPercobaan223 {
 
     float total = (uas*0.4F) + (uts*0.3F) + (kuis*0.1F) + (tugas*0.2F);
 
-    String message = total < 65 ? "Remidi" : "Tidak remidi";
-
-    System.out.println("Nilai akhir = " +total+ " sehingga " +message);
-
+    System.out.println("Nilai total: " +total);
+    
+    if (total>80){
+        System.out.println("Nilai akhir: A");
+    }
+    else if (total>73){  
+        System.out.println("Nilai akhir : B+");    
+    }
+    else if (total>65){
+        System.out.println("Nilai akhir: B");
+    }
+    else if (total>60){
+        System.out.println("Nilai akhir: C+");
+    }
+    else if (total>50){
+        System.out.println("Nilai akhir: C");
+    }
+    else if (total>39){
+        System.out.println("Nilai akhir: D");
+    }
+    else {
+        System.out.println("NIlai akhir: E");
+    }
   }
 }
