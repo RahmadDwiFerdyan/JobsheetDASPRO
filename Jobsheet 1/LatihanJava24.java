7bbbcbead88c6a2e8d84c56f0c21e6955805c4a5
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-09-29 00:05:35 +0700

    update

---
+++ Jobsheet 1/LatihanJava24.java
@@ -0,0 +1,9 @@
+public class LatihanJava24 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama saya Rahmad Dwi Ferdyan");
+        System.out.println("Saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+        
+    }
+    
+}
---
+++ Jobsheet 1/TUGAS.java
@@ -0,0 +1,14 @@
+/**
+ * TUGAS
+ */
+public class TUGAS {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama        : Rahmad Dwi Ferdyan");
+        System.out.println("Absen       : 24");
+        System.out.println("Kelas       :1H");
+        System.out.println("Prodi       : D4 Teknik Informatika");
+        System.out.println("Jurusan     : Teknologi Informasi");
+    }
+}
