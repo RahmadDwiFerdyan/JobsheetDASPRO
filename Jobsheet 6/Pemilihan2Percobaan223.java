bc9d2d60d35ea9c97b7c60f67c278414a8096417
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-10-03 09:39:45 +0700

    modifikasi percobaan 2

--- Jobsheet 6/Pemilihan2Percobaan223.java
+++ Jobsheet 6/Pemilihan2Percobaan223.java
@@ -6,11 +6,11 @@ public class Pemilihan2Percobaan223 {
 
     float sudut1, sudut2, sudut3, totalSudut;
 
-    System.out.println("Masukkan sudut 1: ");
+    System.out.print("Masukkan sudut 1: ");
         sudut1=input23.nextFloat();
-    System.out.println("Masukkan sudut 2: ");
+    System.out.print("Masukkan sudut 2: ");
         sudut2=input23.nextFloat();
-    System.out.println("Masukkan sudut 3: ");
+    System.out.print("Masukkan sudut 3: ");
         sudut3=input23.nextFloat();
 
     totalSudut = sudut1 + sudut2 + sudut3;
@@ -18,10 +18,13 @@ public class Pemilihan2Percobaan223 {
     if (totalSudut == 180) {
         if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
             System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+        else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60))
+            System.out.println("Segitiga tersebut adalah  segitiga sama sisi");
+        else if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3))
+            System.out.println("Segitiga tersebut adalah segitiga sama kaki");
         else
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+            System.out.println("Segitiga tersebut adalah segitiga sembarang");
     } else
         System.out.println("Bukan Segitiga");
-
     }
 }
