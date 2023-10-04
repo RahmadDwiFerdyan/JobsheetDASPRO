86955a97a4c726d4c5c243eaa000c2009c70fb45
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-10-04 07:36:33 +0700

    oiginal

--- Jobsheet 6/Pemilihan2Percobaan123.java
+++ Jobsheet 6/Pemilihan2Percobaan123.java
@@ -6,21 +6,13 @@ public class Pemilihan2Percobaan123 {
 
     int tahun;
 
-    System.out.println("Masukkan Tahun: ");
+    System.out.print("Masukkan Tahun: ");
     tahun = input23.nextInt();
 
     if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0){
+        if ((tahun % 100) != 0)
         System.out.println("Tahun Kabisat");
-        } else if (tahun % 100 == 0){
-            if (tahun % 400 == 0)
-            System.out.println("Tahun Kabisat");
-            else 
-            System.out.println("Bukan Tahun Kabisat");
-        } else {
+    } else
         System.out.println("Bukan Tahun Kabisat");
-        }
-  } else
-        System.out.println("Bukan Tahun Kabisat");  
     }
 }
