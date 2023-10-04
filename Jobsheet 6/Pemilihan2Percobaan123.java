396aa91645f121b77ad6d5b7eb9aa4f3d01bd3ee
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-10-04 07:37:24 +0700

    modifikasi 1

--- Jobsheet 6/Pemilihan2Percobaan123.java
+++ Jobsheet 6/Pemilihan2Percobaan123.java
@@ -12,6 +12,8 @@ public class Pemilihan2Percobaan123 {
     if ((tahun % 4) == 0) {
         if ((tahun % 100) != 0)
         System.out.println("Tahun Kabisat");
+        else 
+        System.out.println("Bukan Tahun Kabisat");
     } else
         System.out.println("Bukan Tahun Kabisat");
     }
