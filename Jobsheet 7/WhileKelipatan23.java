8c46baf044ebd8c5ce2cd362ce92440f56f29863
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-10-11 11:21:43 +0700

    modifikasi while percobaan 1

--- Jobsheet 7/WhileKelipatan23.java
+++ Jobsheet 7/WhileKelipatan23.java
@@ -6,7 +6,22 @@ public class WhileKelipatan23 {
 
     int kelipatan, counter=0;
     double jumlah=0, rerata;
-
-    }
     
+    System.out.println("Masukkan bilangan kelipatan (1-9): ");
+    kelipatan=input23.nextInt();
+
+    int i = 1;
+    while (i <= 50) {
+        if (i % kelipatan == 0) {
+            jumlah += i;
+            counter++;
+        
+    } i++;
+    }
+    rerata = jumlah / counter;
+    System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+    System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %f\n", kelipatan, jumlah);
+    System.out.printf("Rata-rata dari bilangan-bilangan kelipatan %d adalah %f", kelipatan, rerata);
+
+ }
 }
