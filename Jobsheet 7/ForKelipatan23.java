1204f9a2b85511ce4a915fdbb91bb56c90efacbf
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-10-11 11:05:37 +0700

    modifikasi percobaan 1

--- Jobsheet 7/ForKelipatan23.java
+++ Jobsheet 7/ForKelipatan23.java
@@ -5,7 +5,7 @@ public class ForKelipatan23 {
     Scanner input23 = new Scanner(System.in);
 
     int kelipatan, counter=0;
-    double jumlah=0, rerata;
+    float jumlah=0, rerata;
 
     System.out.println("Masukkan bilangan kelipatan (1-9): ");
     kelipatan=input23.nextInt();
@@ -21,8 +21,8 @@ public class ForKelipatan23 {
     rerata = jumlah / counter;
 
     System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-    System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %f\n", kelipatan, jumlah);
-    System.out.printf("Rata-rata dari bilangan-bilangan kelipatan %d adalah %f", kelipatan, rerata);
+    System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %.0f\n", kelipatan, jumlah);
+    System.out.printf("Rata-rata dari bilangan-bilangan kelipatan %d adalah %.2f", kelipatan, rerata);
 
   } 
 }
---
+++ Jobsheet 7/WhileKelipatan23.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class WhileKelipatan23 {
+
+    public static void main(String[] args) {
+    Scanner input23 = new Scanner(System.in);
+
+    int kelipatan, counter=0;
+    double jumlah=0, rerata;
+
+    }
+    
+}
