67462f3fca7b1810c33cf2ae53c759c94ef6de45
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-11-08 08:38:24 +0700

    percobaan 5 modif 2

--- Jobsheet 11/NestedLoop_2341720122.java
+++ Jobsheet 11/NestedLoop_2341720122.java
@@ -3,7 +3,7 @@ public class NestedLoop_2341720122 {
 
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
+    
     double [][] temps  = new double [5][7];
 
     for (int i=0; i<temps.length; i++){
@@ -15,11 +15,17 @@ public class NestedLoop_2341720122 {
         System.out.println();
     }
 
+
     for (int i=0; i<temps.length; i++){
         System.out.print("Kota ke-" + (i+1) + ": ");
+        double total = 0;
         for (double printTemps : temps[i]){
+            total += printTemps;
             System.out.print(printTemps + " ");
         }
+        double rerata = total/ temps[i].length;
+        System.out.println();
+        System.out.printf("  >>Rata-rata suhu kota ke-" + (i+1) +": %.2f", rerata);
         System.out.println();
     }
   }
