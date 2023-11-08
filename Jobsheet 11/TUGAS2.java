b5443eb7ae5bba5ec142ddf8609332afcc1598b0
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-11-08 09:16:50 +0700

    tugas

--- Jobsheet 11/TUGAS2.java
+++ Jobsheet 11/TUGAS2.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class TUGAS2 {
-    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int i, j, jumlah;
+    System.out.print("Masukkan jumlah N = ");
+    jumlah = sc.nextInt();
+
+    for (i=0; i<jumlah; i++){
+        for (j=jumlah; j>i; j--){
+            System.out.print("*");
+        }
+    System.out.println();
+    }
+
+    }
 }
