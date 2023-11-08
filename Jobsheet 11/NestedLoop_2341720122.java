942e5c7c5e2afc793273e4aa5ed4c08744214802
Rahmad Dwi Ferdyan <rahmadferdyan440@gmail.com>
2023-11-08 08:37:54 +0700

    percobaan 5 modif 1

--- Jobsheet 11/NestedLoop_2341720122.java
+++ Jobsheet 11/NestedLoop_2341720122.java
@@ -14,10 +14,11 @@ public class NestedLoop_2341720122 {
         }
         System.out.println();
     }
+
     for (int i=0; i<temps.length; i++){
         System.out.print("Kota ke-" + (i+1) + ": ");
-        for (int j=0; j<temps[0].length; j++){
-            System.out.print(temps[i][j] + " ");
+        for (double printTemps : temps[i]){
+            System.out.print(printTemps + " ");
         }
         System.out.println();
     }
